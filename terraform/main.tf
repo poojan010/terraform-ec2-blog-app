49ba009eb22f94d358f832c45f36e81a7795ec0c
Poojan Bhatt <pdbhatt010@gmail.com>
2025-06-18 16:28:49 +0530

    feat: :sparkles: update ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,12 @@
 data "aws_ami" "amazon_linux" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-*-x86_64"]
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["137112412989"] # Official Amazon account
 }
 
 resource "aws_instance" "blog_server" {
